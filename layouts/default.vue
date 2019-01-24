f2f0e8e06a88a6eae164bb0b0ee645e75b904f45
Brandon Satrom <bsatrom@gmail.com>
2019-01-24 15:16:18 -0600

    added top nav; closes #7

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <header>
+      <nav class="flex w-75 justify-around f4 pa1">
+        <nuxt-link class="link underline-hover" to="/">Public</nuxt-link>
+        <nuxt-link class="link underline-hover" to="/official">Official</nuxt-link>
+        <nuxt-link class="link underline-hover" to="/verified">Verified</nuxt-link>
+        <nuxt-link class="link underline-hover" to="/featured">Featured</nuxt-link>
+      </nav>
+    </header>
+    <nuxt></nuxt>
+  </div>
+</template>
+
+<style>
+.nuxt-link-exact-active {
+  font-weight: bold;
+  text-decoration: underline;
+}
+</style>
