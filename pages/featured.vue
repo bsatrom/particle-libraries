2ec8cbeb9d9acde24cf1426ef238d34fd673438f
Brandon Satrom <bsatrom@gmail.com>
2019-01-24 15:07:35 -0600

    added filter pages; closes #6

---
+++ pages/featured.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Libraries></Libraries>
+  </div>
+</template>
+
+<script>
+import Libraries from "~/components/Libraries.vue";
+
+export default {
+  components: {
+    Libraries
+  },
+  async fetch({ store }) {
+    await store.dispatch("LOAD_LIBRARIES", "featured");
+  }
+};
+</script>
