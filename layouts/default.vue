e4bf806a9f22d8c13b5d64210ef3194702a037fa
Brandon Satrom <bsatrom@gmail.com>
2019-01-26 08:31:45 -0600

    added sticky nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,21 @@
 <template>
-  <div class="pa3">
-    <header>
-      <b-navbar class="parti-header" toggleable="md" type="dark">
-        <b-navbar-brand href="/">Particle Firmware Libraries Registry</b-navbar-brand>
+  <div>
+    <b-navbar class="parti-header" toggleable="md" type="dark" sticky>
+      <b-navbar-brand href="/">Particle Firmware Libraries Registry</b-navbar-brand>
 
-        <b-navbar-nav>
-          <b-nav-item to="/" exact>Public</b-nav-item>
-          <b-nav-item to="/official">Official</b-nav-item>
-          <b-nav-item to="/verified">Verified</b-nav-item>
-        </b-navbar-nav>
-        <b-navbar-nav class="ml-auto">
-          <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-          </b-nav-form>
-          <b-nav-item to="/login">Login</b-nav-item>
-        </b-navbar-nav>
-      </b-navbar>
-    </header>
+      <b-navbar-nav>
+        <b-nav-item to="/" exact>Public</b-nav-item>
+        <b-nav-item to="/official">Official</b-nav-item>
+        <b-nav-item to="/verified">Verified</b-nav-item>
+      </b-navbar-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-nav-form>
+        <b-nav-item to="/login">Login</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
     <nuxt></nuxt>
   </div>
 </template>
