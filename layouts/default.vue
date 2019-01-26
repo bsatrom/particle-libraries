a2b2c3612604588a5d07a4b94728e3e2c2e60581
Brandon Satrom <bsatrom@gmail.com>
2019-01-26 08:21:50 -0600

    cleaned up top nav; closes #19

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,33 +4,19 @@
       <b-navbar class="parti-header" toggleable="md" type="dark">
         <b-navbar-brand href="/">Particle Firmware Libraries Registry</b-navbar-brand>
 
-        <!--<b-navbar-nav>
-          <b-nav-item>
-            <nuxt-link to="/">Public</nuxt-link>
-          </b-nav-item>
-          <b-nav-item>
-            <nuxt-link to="/official">Official</nuxt-link>
-          </b-nav-item>
-          <b-nav-item>
-            <nuxt-link to="/verified">Verified</nuxt-link>
-          </b-nav-item>
-        </b-navbar-nav>-->
-        <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-        </b-nav-form>
+        <b-navbar-nav>
+          <b-nav-item to="/" exact>Public</b-nav-item>
+          <b-nav-item to="/official">Official</b-nav-item>
+          <b-nav-item to="/verified">Verified</b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
+          <b-nav-item to="/login">Login</b-nav-item>
+        </b-navbar-nav>
       </b-navbar>
-      <b-nav fill class="f4">
-        <b-nav-item>
-          <nuxt-link to="/">Public</nuxt-link>
-        </b-nav-item>
-        <b-nav-item>
-          <nuxt-link to="/official">Official</nuxt-link>
-        </b-nav-item>
-        <b-nav-item>
-          <nuxt-link to="/verified">Verified</nuxt-link>
-        </b-nav-item>
-      </b-nav>
     </header>
     <nuxt></nuxt>
   </div>
@@ -41,6 +27,7 @@ a {
   color: #03aeef;
 }
 
+.router-link-exact-active,
 .nuxt-link-exact-active {
   font-weight: bold;
   text-decoration: underline;
