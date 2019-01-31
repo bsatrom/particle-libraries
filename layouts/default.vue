786fa908c10aee1e164e29c8fe22801afefb173c
Brandon Satrom <bsatrom@gmail.com>
2019-01-31 14:39:07 -0600

    added b-collapse; closes #20

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
     <b-navbar class="parti-header" toggleable="md" type="dark" sticky>
-      <b-navbar-brand href="/">Particle Firmware Libraries Registry</b-navbar-brand>
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-      <b-navbar-nav>
-        <b-nav-item to="/" exact>Public</b-nav-item>
-        <b-nav-item to="/official">Official</b-nav-item>
-        <b-nav-item to="/verified">Verified</b-nav-item>
-      </b-navbar-nav>
-      <b-navbar-nav class="ml-auto">
-        <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-        </b-nav-form>
-        <b-nav-item to="/login">Login</b-nav-item>
-      </b-navbar-nav>
+      <b-navbar-brand to="/">Particle Firmware Libraries Registry</b-navbar-brand>
+
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item to="/" exact>Public</b-nav-item>
+          <b-nav-item to="/official">Official</b-nav-item>
+          <b-nav-item to="/verified">Verified</b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
+          <b-nav-item to="/login">Login</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
     </b-navbar>
     <nuxt></nuxt>
   </div>
